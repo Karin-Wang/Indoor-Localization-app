3c571bec31a19d4a7d1365cfea09326d8e1e2255
dbanyay <banyaydani@gmail.com>
2018-03-22 11:47:32 +0100

    basic motion model working, needs autocorrelation

--- app/src/main/java/com/example/particlefilter/MainActivity.java
+++ app/src/main/java/com/example/particlefilter/MainActivity.java
@@ -38,30 +38,44 @@ import java.lang.Thread;
  * Smart Phone Sensing Example 2. Working with sensors.
  */
 public class MainActivity extends Activity implements SensorEventListener {
-    /**
-     * The sensor manager object.
-     */
+
     private SensorManager sensorManager;
-    /**
-     * The accelerometer.
-     */
+
+
     private Sensor accelerometer;
-    /**
-     * The wifi manager.
-     */
+    private Sensor linearaccelerometer;
+    private Sensor magnetometer;
+    private Sensor stepcounter;
+
+    int stepcount = 0;
+
+
     private WifiManager wifiManager;
 
-    /**
-     * Text fields to show the sensor values.
-     */
-    private TextView titleRssi,textRssi;
 
-    Button buttonStart,buttonClear;
+    private TextView textstep,textaz;
+
+    Button buttonAccRecord;
+    private boolean isRecord = false;
+    private double accmagnitude;
+    private long timestamp;
+    String fileNameAcc;
 
 
 
     String curTime = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
 
+    private final float[] mAccelerometerReading = new float[3];
+    private final float[] mMagnetometerReading = new float[3];
+
+    private final float[] mRotationMatrix = new float[9];
+    private final float[] mOrientationAngles = new float[3];
+
+
+    private int samplecounter = 0;
+    private double sum = 0;
+
+    private double[] accarray = new double[5];
 
 
 
@@ -72,8 +86,14 @@ public class MainActivity extends Activity implements SensorEventListener {
         setContentView(R.layout.activity_main);
 
         // Create the text views.
-        titleRssi = (TextView) findViewById(R.id.titleRSSI);
-        textRssi = (TextView) findViewById(R.id.textRSSI);
+        textstep = (TextView) findViewById(R.id.textSTEPCOUNT);
+        textaz = (TextView) findViewById(R.id.textROTAZ);
+
+        // create buttons
+        buttonAccRecord = (Button) findViewById(R.id.recordAcc);
+        fileNameAcc = "accData_" + curTime + ".csv";
+
+
 
         // create image view
 
@@ -90,10 +110,10 @@ public class MainActivity extends Activity implements SensorEventListener {
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
         // if the default accelerometer exists
-        if (sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION) != null) {
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
             // set accelerometer
             accelerometer = sensorManager
-                    .getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+                    .getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
             // register 'this' as a listener that updates values. Each time a sensor value changes,
             // the method 'onSensorChanged()' is called.
             sensorManager.registerListener(this, accelerometer,
@@ -103,9 +123,91 @@ public class MainActivity extends Activity implements SensorEventListener {
             // No accelerometer!
         }
 
+        // if the default accelerometer exists
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION) != null) {
+            // set accelerometer
+            linearaccelerometer = sensorManager
+                    .getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+            // register 'this' as a listener that updates values. Each time a sensor value changes,
+            // the method 'onSensorChanged()' is called.
+            sensorManager.registerListener(this, linearaccelerometer,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+
+        } else {
+            // No accelerometer!
+        }
+
+        // if the default magnetometer exists
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) != null) {
+            // set accelerometer
+            magnetometer = sensorManager
+                    .getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+            // register 'this' as a listener that updates values. Each time a sensor value changes,
+            // the method 'onSensorChanged()' is called.
+            sensorManager.registerListener(this, magnetometer,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+
+
+        } else {
+            // No accelerometer!
+        }
+
+        // if the default step counter exists
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR) != null) {
+            // set step counter
+            stepcounter = sensorManager
+                    .getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
+            // register 'this' as a listener that updates values. Each time a sensor value changes,
+            // the method 'onSensorChanged()' is called.
+            sensorManager.registerListener(this, stepcounter,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+
+        } else {
+            // No step counter
+        }
+
 
         // Set the wifi manager
         wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+        // Create a click listener for acc recorder button.
+        buttonAccRecord.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String fileNameAcc;
+
+
+                if (!isRecord) {
+                    isRecord = true;
+                    buttonAccRecord.setText("STOP RECORD");
+
+
+                } else {
+                    isRecord = false;
+
+                    buttonAccRecord.setText("START RECORD");
+                    String curTime = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date()); // time at first call
+                    fileNameAcc = "accData_" + curTime + ".csv";
+
+                    String row ="Acceleration Y, Accelearion Z, timestamp\n";   // write header to csv file
+
+                    try {
+                        File sdCard = Environment.getExternalStorageDirectory();
+                        File dir = new File(sdCard.getAbsolutePath() + "/localization");
+                        dir.mkdir();
+
+                        File file = new File(dir, fileNameAcc);
+                        FileOutputStream f = new FileOutputStream(file,true);
+                        f.write(row.getBytes());
+                        f.flush();
+                        f.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
 
     }
 
@@ -113,6 +215,8 @@ public class MainActivity extends Activity implements SensorEventListener {
     protected void onResume() {
         super.onResume();
         sensorManager.registerListener(this, accelerometer,
+                SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, magnetometer,
                 SensorManager.SENSOR_DELAY_NORMAL);
     }
 
@@ -130,5 +234,110 @@ public class MainActivity extends Activity implements SensorEventListener {
     @Override
     public void onSensorChanged(SensorEvent event) {
 
+        if (event.sensor == accelerometer) {
+            System.arraycopy(event.values, 0, mAccelerometerReading,
+                    0, mAccelerometerReading.length);
+            updateOrientationAngles();
+
+        }
+        else if (event.sensor == magnetometer) {
+            System.arraycopy(event.values, 0, mMagnetometerReading,
+                    0, mMagnetometerReading.length);
+            updateOrientationAngles();
+        }
+
+       /* else if (event.sensor == stepcounter) {
+
+            stepcount++;
+            textstep.setText("Step Counter : "+stepcount);
+
+        }*/
+
+        else if (event.sensor == linearaccelerometer) {
+
+            accmagnitude = Math.sqrt(event.values[0]*event.values[0]+event.values[2]*event.values[2]);
+            timestamp = event.timestamp;
+            if(isRecord)writeAccValuesCSV(accmagnitude,timestamp);
+            motionSensor(accmagnitude);
+
+        }
     }
+
+    public void writeAccValuesCSV(double accmagnitude,long timestamp) {
+
+        try {
+
+            File sdCard = Environment.getExternalStorageDirectory();
+            File dir = new File(sdCard.getAbsolutePath() + "/localization");
+            dir.mkdir();
+
+            File file = new File(dir, fileNameAcc);
+            FileOutputStream f = new FileOutputStream(file,true);
+
+            String row = String.valueOf(accmagnitude)+ " , "+timestamp+"\n";
+
+            try {
+                f.write(row.getBytes());
+                f.flush();
+                f.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public void updateOrientationAngles() {
+        // Update rotation matrix, which is needed to update orientation angles.
+        SensorManager.getRotationMatrix(mRotationMatrix, null,
+                mAccelerometerReading, mMagnetometerReading);
+
+        SensorManager.getOrientation(mRotationMatrix, mOrientationAngles);
+
+        textaz.setText("Azimuth: "+mOrientationAngles[0]/3.1415*180);
+
+    }
+
+    public void motionSensor(double accmagnitude) {
+
+        int windowsize = 5;
+
+        double deviation = 0;
+        double mean;
+        double WALK_LIMIT = 0.15;
+        double STEP_LIMIT = 0.08;
+
+
+
+        if(samplecounter == windowsize-1) {
+            // do  standard deviation
+            mean = sum/windowsize;
+            for (int i=0; i<windowsize; i++) {
+                deviation = Math.abs(accarray[i]-mean)/windowsize;
+            }
+
+            if (deviation > WALK_LIMIT) {
+                textstep.setText("Walking");
+            }
+            if (deviation < STEP_LIMIT) textstep.setText("Standing");
+
+            sum = 0;
+            samplecounter = 0;
+
+
+        }
+        else {
+            accarray[samplecounter] = accmagnitude;
+            sum += accmagnitude;
+            samplecounter++;
+
+        }
+
+    }
+
 }
