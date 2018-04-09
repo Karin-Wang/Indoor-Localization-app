1d3f1b2a779706a79dd58fcdf4923fd8a84b752e
Xinyue Wang <x.wang-25@student.tudelft.nl>
2018-04-09 17:07:23 +0200

    bayesian with direction v1.0

--- app/src/main/java/com/example/particlefilter/Bayesian.java
+++ app/src/main/java/com/example/particlefilter/Bayesian.java
@@ -29,10 +29,11 @@ public class Bayesian {
 
     Map<String, float[][]> radioMap = new HashMap<>();
 
-    Map<String, float[][]> radioMapN = radioMap; // TODO change to actual radiomaps
-    Map<String, float[][]> radioMapE = radioMap;
-    Map<String, float[][]> radioMapS = radioMap;
-    Map<String, float[][]> radioMapW = radioMap;
+    Map<String, float[][]> radioMapN = new HashMap<>();
+    Map<String, float[][]> radioMapE = new HashMap<>();
+    Map<String, float[][]> radioMapS = new HashMap<>();
+    Map<String, float[][]> radioMapW = new HashMap<>();
+    Map<String, float[][]>[] radioMap_All = new Map[]{radioMapN, radioMapE, radioMapS, radioMapW};
 
 
     private double threshold = 0.9;
@@ -54,12 +55,6 @@ public class Bayesian {
 
     public void getRadioMap(BufferedReader reader){
         try {
-////            InputStream rawRes = context.getAssets().open("radio.extension");
-//            Resources r = this.getContext().getResources();
-//            InputStream rawRes = getResources().openRawResource(R.raw.radio_map);
-////            Reader r = new BufferedReader(new InputStreamReader(rawRes, "UTF8"));
-//            BufferedReader reader = new BufferedReader(new InputStreamReader(rawRes, "UTF8"));//换成你的文件名
-//            reader.readLine();//第一行信息，为标题信息，不用，如果需要，注释掉
             String line = null;
             while((line=reader.readLine())!=null){
                 String ap = line;
@@ -67,20 +62,40 @@ public class Bayesian {
                 for (int i=0; i<19; i++){
                     line=reader.readLine();
                     String[] item = line.split("，");//CSV格式文件为逗号分隔符文件，这里根据逗号切分
-//                    Log.d("item", item[0].toString());
                     String[] a = item[0].split(",");
-
 
                     curr[i][0] = Float.parseFloat(a[0].substring(1));
                     curr[i][1] = Float.parseFloat(a[1].substring(0,a[1].length()-1));
-//                    curr[i][0] = Float.parseFloat(item[0]);
-//                    curr[i][1] = Float.parseFloat(item[1]);
-//                    Log.d("1", curr[i].toString());
                 }
                 this.radioMap.put(ap, curr);
             }
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    public void getAllRadioMap(BufferedReader[] readers) {
+        int count = 0;
+        for (BufferedReader reader : readers) {
+            try {
+                String line = null;
+                while ((line = reader.readLine()) != null) {
+                    String ap = line;
+                    float[][] curr = new float[19][2];
+                    for (int i = 0; i < 19; i++) {
+                        line = reader.readLine();
+                        String[] item = line.split("，");//CSV格式文件为逗号分隔符文件，这里根据逗号切分
+                        String[] a = item[0].split(",");
+
+                        curr[i][0] = Float.parseFloat(a[0].substring(1));
+                        curr[i][1] = Float.parseFloat(a[1].substring(0, a[1].length() - 1));
+                    }
+                    this.radioMap_All[count].put(ap, curr);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            count++;
         }
     }
 
@@ -176,20 +191,63 @@ public class Bayesian {
         double[] average_overvall_post = new double[19];
         for (int i = 0; i < 19; i++) {
             average_overvall_post[i] = overall_post[i] / sum1;
-            Log.d("average_overvall_post", String.valueOf(average_overvall_post[i]));
+//            Log.d("average_overvall_post", String.valueOf(average_overvall_post[i]));
         }
         Object[] fdmax = findMax(average_overvall_post);
         predict = (int) fdmax[1];
         proba = (double) fdmax[0];
-////            i++; //下一组数据
-//        iteration++;
-//        if (iteration > 10) {
-//            return fdmax;
-//        }
-//        predict = findMax1(average_overvall_post);
-        Log.d("proba", String.valueOf((Double) (fdmax[0])));
-        Log.d("predict", fdmax[1].toString());
-//        return predict;
+        return fdmax;
+    }
+
+    public Object[] bayes_new(Map<String, Integer> test_data) {
+        prior = post.clone();
+        j = 0;
+        double[] overall_post = new double[19];
+        for (int i = 0; i < 19; i++) {
+            overall_post[i] = 0;
+        }
+        List<Entry<String, Integer>> sorted_test_data = sortMap(test_data);
+        int sizet = 30;
+        if (test_data.size()<30){
+            sizet = test_data.size();
+        }
+        for (int item = 0; item < sizet; item++) {
+            double[] pdf = new double[19];
+            for (int i = 0; i < 19; i++) {
+                pdf[i] = 0;
+            }
+            for (int cell = 0; cell < 19; cell++) {
+                String AP = sorted_test_data.get(item).getKey();
+                if (radioMap.get(AP)[cell][1] == 0.0){
+                    pdf[cell] = 0.0;
+                }
+                else {
+                    NormalDistribution d = new NormalDistribution(radioMap.get(AP)[cell][0], radioMap.get(AP)[cell][1]);
+                    pdf[cell] = d.density(normalization((test_data.get(AP))));
+                    if (Double.isNaN(pdf[cell])) {
+                        pdf[cell] = 0.0;
+                    }
+                }
+//                Log.d("proba1", String.valueOf(pdf[cell]));
+            }
+            post[j] = matrixOperation(prior[j], pdf, "*");
+
+            overall_post = matrixOperation(post[j], overall_post, "+");
+            j += 1;
+        }
+        double sum1 = 0;
+        for (int i = 0; i < 19; i++) {
+            sum1 += overall_post[i];
+//            Log.d("overall", String.valueOf(sum1));
+        }
+        double[] average_overvall_post = new double[19];
+        for (int i = 0; i < 19; i++) {
+            average_overvall_post[i] = overall_post[i] / sum1;
+//            Log.d("average_overvall_post", String.valueOf(average_overvall_post[i]));
+        }
+        Object[] fdmax = findMax(average_overvall_post);
+        predict = (int) fdmax[1];
+        proba = (double) fdmax[0];
         return fdmax;
     }
 
@@ -223,26 +281,26 @@ public class Bayesian {
             return res;
         }
 
-    public void chooseRadioMap(double angle){
-
-        // double initialAngle = MyView.getInitialAngle();
-
-        double Q1 = -1.41; // value at pi/8
-        double Q2 = 0.15; // value at 3/pi*8
-        double Q3 = 1.72; // value at 5*pi/8
-
-        if (angle > -Math.PI && angle < Q1){
-            radioMap = radioMapN;
-            Log.d("North", "");
-        } else if (angle > Q1 && angle < Q2){
-            radioMap = radioMapE;
-            Log.d("East", "");
-        } else if (angle > Q2 && angle < Q3){
-            radioMap = radioMapS;
-            Log.d("South", "");
-        }
-
-    }
+//    public void chooseRadioMap(double angle){
+//
+//        // double initialAngle = MyView.getInitialAngle();
+//
+//        double Q1 = -1.41; // value at pi/8
+//        double Q2 = 0.15; // value at 3/pi*8
+//        double Q3 = 1.72; // value at 5*pi/8
+//
+//        if (angle > -Math.PI && angle < Q1){
+//            radioMap = radioMapN;
+//            Log.d("North", "");
+//        } else if (angle > Q1 && angle < Q2){
+//            radioMap = radioMapE;
+//            Log.d("East", "");
+//        } else if (angle > Q2 && angle < Q3){
+//            radioMap = radioMapS;
+//            Log.d("South", "");
+//        }
+//
+//    }
 
 }
 
